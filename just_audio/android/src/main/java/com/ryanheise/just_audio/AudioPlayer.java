cc7123cb909fe43f88b958c016fc4c918ff1df98
Aroha Media LTD <tech@aroha.ae>
2025-10-07 13:22:49 +0100

    Update AudioPlayer.java
    ENhanced logging

--- just_audio/android/src/main/java/com/ryanheise/just_audio/AudioPlayer.java
+++ just_audio/android/src/main/java/com/ryanheise/just_audio/AudioPlayer.java
@@ -310,40 +310,46 @@ public class AudioPlayer implements MethodCallHandler, Player.Listener {
 
     @Override
 	public void onTracksChanged(Tracks tracks) {
-	
-		Log.d(TAG, "onTracksChanged called");
-	
-	  for (int i = 0; i < tracks.getGroups().size(); i++) {
-		TrackGroup trackGroup = tracks.getGroups().get(i).getMediaTrackGroup();
-		for (int j = 0; j < trackGroup.length; j++) {
-		  Metadata metadata = trackGroup.getFormat(j).metadata;
-		  if (metadata != null) {
-			for (int k = 0; k < metadata.length(); k++) {
-			  final Metadata.Entry e = metadata.get(k);
-	
-			  if (e instanceof IcyHeaders) {
-				icyHeaders = (IcyHeaders) e;
-				broadcastImmediatePlaybackEvent();
-			  } else if (e instanceof TextInformationFrame) {
-				final TextInformationFrame f = (TextInformationFrame) e;
-				final String id = f.id != null ? f.id.toUpperCase() : "";
-				@SuppressWarnings("deprecation") final String value = f.value;
-				if (value == null) continue;
-				Log.d(TAG, "[TM] (FMT) " + id + " = " + value);
-	
-				if ("TIT2".equals(id)) {
-				  emitTimedMetadata(mapOf("type","id3","id","TIT2","value", value));
-				} else if ("TPE1".equals(id)) {
-				  emitTimedMetadata(mapOf("type","id3","id","TPE1","value", value));
-				} else if ("TXXX".equals(id)) {
-				  final String desc = f.description != null ? f.description : "";
-				  emitTimedMetadata(mapOf("type","id3-txxx","description", desc,"value", value));
+		android.util.Log.d(TAG, "=== onTracksChanged called ===");
+		android.util.Log.d(TAG, "Number of track groups: " + tracks.getGroups().size());
+		
+		for (int i = 0; i < tracks.getGroups().size(); i++) {
+			TrackGroup trackGroup = tracks.getGroups().get(i).getMediaTrackGroup();
+			android.util.Log.d(TAG, "Track group " + i + " has " + trackGroup.length + " tracks");
+			
+			for (int j = 0; j < trackGroup.length; j++) {
+				Format format = trackGroup.getFormat(j);
+				android.util.Log.d(TAG, "  Track " + j + ": " + format.sampleMimeType + " (metadata: " + (format.metadata != null ? format.metadata.length() + " entries" : "null") + ")");
+				
+				Metadata metadata = format.metadata;
+				if (metadata != null) {
+					for (int k = 0; k < metadata.length(); k++) {
+						final Metadata.Entry e = metadata.get(k);
+						android.util.Log.d(TAG, "    Metadata entry: " + e.getClass().getSimpleName());
+						
+						if (e instanceof IcyHeaders) {
+							icyHeaders = (IcyHeaders) e;
+							broadcastImmediatePlaybackEvent();
+						} else if (e instanceof TextInformationFrame) {
+							final TextInformationFrame f = (TextInformationFrame) e;
+							final String id = f.id != null ? f.id.toUpperCase() : "";
+							@SuppressWarnings("deprecation") final String value = f.value;
+							if (value == null) continue;
+							android.util.Log.d(TAG, "[TM] (FMT) " + id + " = " + value);
+							
+							if ("TIT2".equals(id)) {
+								emitTimedMetadata(mapOf("type","id3","id","TIT2","value", value));
+							} else if ("TPE1".equals(id)) {
+								emitTimedMetadata(mapOf("type","id3","id","TPE1","value", value));
+							} else if ("TXXX".equals(id)) {
+								final String desc = f.description != null ? f.description : "";
+								emitTimedMetadata(mapOf("type","id3-txxx","description", desc,"value", value));
+							}
+						}
+					}
 				}
-			  }
 			}
-		  }
 		}
-	  }
 	}
 
     private boolean updatePositionIfChanged() {
